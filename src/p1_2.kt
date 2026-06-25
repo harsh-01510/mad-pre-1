eadd7843b670b47922f987a9e954484bc59df55a
Harsh <sutharharsh1510@gmail.com>
2026-06-25 14:23:18 +0530

    intial

---
+++ src/p1_2.kt
@@ -0,0 +1,18 @@
+fun main(){
+ var i  = 30000
+ var j = i.toDouble()
+
+    println("i is = $i")
+    println("j is = $j")
+
+ val s1 = "30500"
+ val k= s1.toInt()
+     println("s1 is $s1")
+     println("k is $k")
+
+  var l = "40000000"
+  var m = l.toInt()
+
+  println("l is $l")
+  println("m is $m")
+}
