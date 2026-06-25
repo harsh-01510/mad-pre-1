bae1732314b808248f0b814649b2ee554fd191e6
Harsh <sutharharsh1510@gmail.com>
2026-06-25 14:06:49 +0530

    intial

---
+++ src/p1_1.kt
@@ -0,0 +1,31 @@
+fun main (){
+   var i : Int =10
+   var j : Float =20f
+   var k : Char ='A'
+   var l : String ="Hello kotlin"
+   var m : Boolean =true
+   var n : Double = 10.10
+   var o : Long = 11234444
+   var p : Short = -12
+   var q : Byte = 11
+   val z = 20000
+
+    println("Int value is = "+i)
+    println("Float value is = "+j)
+    println("Char value is = "+k)
+    println("String value is = "+l)
+    println("Boolean value is = "+m)
+    println("Double value is = "+n)
+    println("Long value is = "+o)
+    println("Short value is = "+p)
+    println("Byte value is = "+q)
+    println("z is ="+ z)
+
+    print("Z is = "+z)
+    println("z is = $z")
+    println("z is = ${z}")
+    System.out.println("Z is = "+ z)
+
+
+
+}
