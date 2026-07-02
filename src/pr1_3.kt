2f2d210259fd93ae8ccb9388aa44b294981047b1
Harsh <sutharharsh1510@gmail.com>
2026-07-02 13:08:59 +0530

    intial

---
+++ src/pr1_3.kt
@@ -0,0 +1,45 @@
+fun main(){
+    var s1 : String ? = "23"
+
+    //val a1 : Int  = s1 !!.toInt()
+
+    //println(a1)
+//    print("Enter the number :")
+//    var s2 = readln().toInt()
+//    println(s2)
+
+    print("Student Enroll number :")
+    var a1 : Long =readln().toLong()
+
+    print("Student name :")
+    var a2 : String =readln().toString()
+
+    print("Student Branch :")
+    var a3 : String =readln().toString()
+
+    print("Student calss :")
+    var a4 : String =readln().toString()
+
+    print("Student Batch :")
+    var a5 : String =readln().toString()
+
+    print("Student Collage name :")
+    var a6 : String =readln().toString()
+
+    print("Student University name :")
+    var a7 : String =readln().toString()
+
+    print("Student Age :")
+    var a8 : Int =readln().toInt()
+
+    println("Student Enroll number : $a1")
+    println("Student name : $a2")
+    println("Student Branch : $a3")
+    println("Student Class : $a4")
+    println("Student Batch : $a5")
+    println("Student Collage name : $a6")
+    println("Student University name : $a7")
+    println("Student Age : $a8")
+
+
+}
